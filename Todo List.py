5371e2e9bdf3d465be34472b70db365b1ef20122
Daniya Alam <49856315+Daniyal56@users.noreply.github.com>
2019-06-26 11:03:49 +0500

    Update Todo List.py

--- Todo List.py
+++ Todo List.py
@@ -1,8 +1,10 @@
 print('=============================================== ToDoList ===============================================')
 
+#Getting Input from User
 words = input('Enter your word to make a list : ').title()
 list = words.split()
 
+#Checking Condition
 while True:
   print('Please select below options')
   print('if you want to add more, Please write : Add ')
@@ -12,9 +14,10 @@ while True:
   print('if you want to exit from the Program, Please write : Exit ')
   
   user_input = input('Please write here ....')
-
+  
+    #Adding more on list
   if user_input == 'Add':
-  #Adding more on list
+
     add_more = input('Enter your Word to add more on the list : ').title()
     print(f'You have added word {add_more} into your list')
     list.append(add_more)
@@ -25,8 +28,9 @@ while True:
       print('please write properly...')
       continue
     
+    #Deleting By Index Number
   elif user_input == 'del by Indexing Number':
-  #Deleting By Index Number
+  
     By_Index_number = int(input('Enter your Word to delete from the list : '))
     del list[By_Index_number]
     print(f'You have removed word {By_Index_number} into your list')
@@ -36,9 +40,10 @@ while True:
     else:
       print('please write properly...')
       continue
-    
-  elif user_input == 'Get List' or 'list':
+   
   #Deleting By Index Number
+  elif user_input == 'Get List' or 'list':
+  
     print(list)
     user_input = input('Please write here if you want to do samething OR someother thing.... in Yes Or NO : ').title()
     if user_input == 'Yes':
@@ -47,8 +52,9 @@ while True:
       break
       print('Thank you for using')
   
-  elif user_input == 'del by name':
   #Deleting By String
+  elif user_input == 'del by name':
+  
     By_string = input('Enter your Word to delete by string Method : ')
     str(list.remove(By_string))
     print(f'You have removed word {By_string} into your list')
@@ -60,10 +66,11 @@ while True:
       break
       print('Thank you for using')
   
+  #Execute from Program
   elif user_input == 'Exit' or 'exit' or 'No' or 'no':
     print('Thank you for using')
     break
-  
+ 
   else:
     print('Please write properly')
 
